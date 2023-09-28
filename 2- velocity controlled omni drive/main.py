48f98035315a358213fdad33850b386063eb4c31
abhijeettomy <abhijeettomy@iisc.ac.in>
2023-09-28 17:09:53 +0530

    trying stablebaseline3

--- 2- velocity controlled omni drive/main.py
+++ 2- velocity controlled omni drive/main.py
@@ -1,5 +1,32 @@
-from stable_baselines3.common.env_checker import check_env
 import Robot_env
-env = Robot_env.RobotEnv()
-# It will check your custom environment and output additional warnings if needed
-check_env(env)
+import numpy as np
+import matplotlib as plt
+from stable_baselines3 import PPO, A2C, DQN
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+# Instantiate the env
+#vec_env = make_vec_env(Robot_env.RobotEnv, n_envs=1)
+
+model = PPO("MlpPolicy",Robot_env.RobotEnv,verbose=1)
+
+best = -100
+
+ 
+
+plot_reward = []    
+for i in range(400):
+  model.learn(total_timesteps=2500)
+  temp = evaluate_policy(model,model.env,n_eval_episodes=5)
+  print("reward", temp, flush = True)
+  plot_reward.append(temp)
+  if(temp[0]>best):
+    model.save("./model_best/omnirobot")
+    best = temp[0]
+  if(i%10==0):
+    model.save("./model/omnirobot")
+
+np.save('fetch_reach.npy',np.array(plot_reward))
+
+ 
+
+model.save("./model/fetchreach")
