1b005e623e5e70e7406050f0d0a584b23ef00655
abhijeettomy <abhijeettomy@iisc.ac.in>
2023-09-28 12:13:29 +0530

    created a custom gym environment

--- 2- velocity controlled omni drive/Robot-env.py
+++ 2- velocity controlled omni drive/Robot-env.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import gym
 from gym.spaces import Box
+from scipy.integrate import odeint
 
 class RobotEnv(gym.Env):
     def __init__(self):
@@ -15,44 +16,121 @@ class RobotEnv(gym.Env):
                                       high=np.array([6.58,4.63,math.pi])) #upper bounds of state
         self.epi_len = 2500
 
-
+    
         #states
         self.x = -3.19
-        self.y = 1.70
+        self.y = 3
         self.theta = 0
         self.state = np.array([self.x,self.y,self.theta])
 
         #RL constants
-        self.timestep = 0
+        self.ep_t = 0
         
         self.time_int = 25/self.epi_len
-        self.t2 = self.timestep*self.time_int
+        # self.t2 = self.timestep*self.time_int
         #reference trajectory
-        xd=[[-1.5+5.8*math.cos(0.24*self.time_int*t+1.5), 3.8*math.sin(0.24*t*self.time_int+1.5)] for t in range(self.epi_len)]
-        self.state_d = np.array(xd)
+        ref_trajectory=[[-1.5+5.8*math.cos(0.24*self.time_int*t+1.5), 5.8*math.sin(0.24*t*self.time_int+1.5)] for t in range(self.epi_len)]
+        self.state_d = np.array(ref_trajectory)
 
-        #Funnel specs ALL VALUES TO BE CHECKED
-        l = np.array([0.1,0.1]) 
-        rho_f = np.array([1.5,1.5])
-        rho_0 = np.array(abs(np.array([self.x,self.y])- self.state_d[0,:]) + 0.2)
-        gamma = np.array([(rho_0 - rho_f)*np.exp(-l*h*t) + rho_f for t in range(self.epi_len)])
+        #Funnel for soft constraint currently values are according to paper
+        l = np.array([0.7,0.7]) 
+        ini_width = 0.2 #higher the value more is the initial funnel width
+        rho_f = np.array([0.2,0.2])
+        rho_0 = np.array(abs(np.array([self.x,self.y])- self.state_d[0,:]) + ini_width )
+        gamma = np.array([(rho_0 - rho_f)*np.exp(-l*self.time_int*t) + rho_f for t in range(self.epi_len)])
         self.lb_soft = self.state_d - gamma
         self.ub_soft = self.state_d + gamma
         self.phi_ini_L = [0,0]
         self.phi_ini_U = [0,0]
 
-    def reset(self):
-        return super().reset()
+        #creating final funnel
+        mu = np.array([3,3])
+        kc = np.array([3,3])
+        self.lb_hard = [-6.58,-4.63]
+        self.ub_hard = [6.58,4.63]
+        t1 = [0,self.time_int]
+        self.phi_L,self.phi_U,self.Lb,self.Ub = [],[],[],[]
+
+        for _ in range(self.epi_len):
+            tr, phi_Lo = odeint(self.bound, self.phi_ini_L, t1, args=(self.lb_soft, self.ub_hard, mu, kc))
+            phi_sol_L = np.abs(phi_Lo) #this condition to be checked i.e,psi(modification signal) is always positive
+            tr, phi_U = odeint(self.bound, self.phi_ini_U, t1, args=(self.lb_hard, self.ub_soft, mu, kc))
+            phi_sol_U = np.abs(phi_U) #this condition to be checked i.e,psi(modification signal) is always positive
+            v = 10
+            lower_bo = np.log(np.exp(v * (self.lb_soft - phi_sol_L)) + np.exp(v * self.lb_hard)) / v
+            upper_bo = -np.log(np.exp(-v * (self.ub_soft + phi_sol_U)) + np.exp(-v * self.ub_hard)) / v
+            self.phi_ini_L = phi_sol_L #will change according to comment in line 55
+            self.phi_ini_U = phi_sol_U
+            # self.phi_L.append(phi_sol_L)
+            # self.phi_U.append(phi_sol_U)
+            self.Lb.append(lower_bo)
+            self.Ub.append(upper_bo)
+
+    def bound(self,phi,t,lb, ub, mu, kc): #####why t???
+        eta = ub-lb
+        dphi_dt = 0.5*(1-np.sign(eta-mu))*(1/(eta+phi))-kc*phi
+        return dphi_dt
+
+
     
     def step(self, action):
+
+        #flag to check if episode is complete or not
+        done = False
+        
+
+        #get new position
         vel_x,vel_y,omega = action[0],action[1],action[2]
-        return super().step(action)
+        x_old,y_old,theta_old = self.x,self.y,self.theta
+
+        self.x = x_old + vel_x * self.time_int
+        self.y = y_old + vel_y * self.time_int
+        self.theta = theta_old + omega * self.time_int
+        self.state = np.array([self.x,self.y,self.theta])
+        # To keep the angle theta between -pi to pi
+        if(self.theta > math.pi or self.theta < -math.pi ):
+           self.theta = ((self.theta + math.pi)%(2*math.pi))-math.pi
+
+        #check if new position is within hard constraint
+        if self.lb_hard[0] <= self.x <= self.ub_hard[0] and self.lb_hard[1] <= self.y <= self.ub_hard[1] :
+            Rew_max = 5
+            #check if within funnel and reward accordingly
+            x_min,y_min = self.Lb[self.ep_t]
+            x_max,y_max = self.Ub[self.ep_t]
+            robust1 = Rew_max - ((self.x - (x_min + x_max)/2)**2)*(4*Rew_max/((x_max-x_min)**2))
+            robust2 = Rew_max - ((self.y - (y_min + y_max)/2)**2)*(4*Rew_max/((y_max-y_min)**2))
+            reward = np.clip(min(robust1, robust2), -10,Rew_max)
+
+        else:
+            #terminate the episode or restart the episode?
+            reward = -100
+            done = True
+        
+        self.ep_t +=1
+
+        if self.ep_t == self.epi_len:
+            done = True  
+
+        info = {}
+        return self.state, reward, done, info
     
     def render(self, mode="human"):
         pass
 
+    def reset(self):
+
+        self.x = -3.19
+        self.y = 3
+        self.theta = 0
+        self.state = np.array([self.x,self.y,self.theta])
+
+        self.ep_t = 0
+
+
+        return self.state
+    
     def  close(self):
-        return super().close()
+        pass
     
     def seed(self, seed=None):
-        return super().seed(seed)
+        pass
